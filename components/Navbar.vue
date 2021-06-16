8695d38f143666d300419edfbfe5998840f17dc6
Jack Otieno <jack.otieno@ymail.com>
2021-06-16 22:58:55 +0300

    Restructured layout page + adding navigation

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navigation">
+        <b-navbar toggleable="lg" type="light" variant="light">
+            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item href="#">Home</b-nav-item>
+                    <b-nav-item href="#">About</b-nav-item>
+                    <b-nav-item href="#">Services</b-nav-item>
+                    <b-nav-item href="#">Portfolio</b-nav-item>
+                    <b-nav-item href="#">Contact Us</b-nav-item>
+                </b-navbar-nav> <!-- ./b-navbar-nav -->
+            </b-collapse> <!-- ./b-collapse -->
+        </b-navbar> <!-- ./b-navbar -->
+    </div> <!-- ./div -->
+</template> <!-- ./template -->
