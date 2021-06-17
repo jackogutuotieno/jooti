bb28149bc85a800ce618f605d687db9c7d73aa0a
Jack Otieno <jack.otieno@ymail.com>
2021-06-17 15:36:12 +0300

    Added production files

---
+++ dist/_nuxt/2fa8bec.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{303:function(t,e,n){"use strict";n.r(e);var c={},r=n(56),component=Object(r.a)(c,(function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"container"},[e("div")])}],!1,null,null,null);e.default=component.exports}}]);
