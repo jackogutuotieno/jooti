f3a01331da6a0bc2ada082b65c8b1146b5a7649e
Jack Otieno <jack.otieno@ymail.com>
2021-06-17 16:04:45 +0300

    Added scss files + structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    // SCSS file in the project
+    '@/assets/scss/styles.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
