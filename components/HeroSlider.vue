8afbef923b321e97c3b12240894fa847278b05ee
Jack Otieno <jack.otieno@ymail.com>
2021-06-19 15:21:40 +0300

    Plugged dummy data to hero slider

--- components/HeroSlider.vue
+++ components/HeroSlider.vue
@@ -8,6 +8,7 @@
                         <div class="text-wrap">
                             <h2>{{ slide.title }}</h2>
                             <p>{{ slide.caption }}</p>
+                            <p><a :href="slide.link" class="btn btn-primary">more</a></p>
                         </div>
                     </div>
                 </div>
@@ -26,14 +27,16 @@
                     url: 'https://placeimg.com/1920/800/any?1', 
                     alt: 'Image one',
                     title: 'The standard Lorem Ipsum passage, used since the 1500s',
-                    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                    caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    link: '/home'
                 },
                 { 
                     id:'2', 
                     url: 'https://placeimg.com/1920/800/any?2', 
                     alt: 'Image two',
                     title: 'Sed ut perspiciatis unde omnis iste natus error sit',
-                    caption: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+                    caption: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                    link: '/about'
                 },
             ],
         }
