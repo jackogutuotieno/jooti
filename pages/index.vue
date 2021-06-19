3bc98266df9b54f3b4fbdc8f60576349386dc117
Jack Otieno <jack.otieno@ymail.com>
2021-06-19 18:29:28 +0300

    Added about section on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
+  <div class="home-page">
     <HeroSlider />
+    <b-container>
+      <b-row class="bg-light">
+        <b-col lg="6">
+          <b-img fluid src="../assets/images/img-01.jpg" alt="About Us"></b-img>
+        </b-col>
+        <b-col lg="6" class="right-col">
+          <div class="title-wrap left--line">
+            <h2>about Us</h2>
+          </div>
+          <div class="content-padded">
+            <p>
+              We strive to go above and beyond for our clients, fostering aelationship built on trust, 
+              confidence and honesty. Maybe it's our family orientation, but we think the most satisfying 
+              part of what we do is that we get toork with so many wonderful people. We're proud that we're 
+              a 10omen-ned business certified by the Women's Business velopment Center (WBDC). We're also 
+              proud that we put people first,ith monthly reward grams and team-building activities throughout 
+              the year. We strive to go above and beyond for our clients.
+            </p>
+            <p>
+              Maybe it’s our family orientation, but we think the most satisfying part of what we do 
+              is that. building activities throughout the year. We strive to go above and bond for our 
+              clients, fostering a relationship built on trust, confidence and nestyaybe it’s  fostering a 
+              relationship our.
+            </p>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
