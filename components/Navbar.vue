5786c889600f83eefe9a0a64782599544a4c7823
Jack Otieno <jack.otieno@ymail.com>
2021-06-20 22:40:04 +0300

    Added fixed navbar position

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="navigation">
-        <b-navbar toggleable="lg" type="light" variant="light">
+        <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
             <b-navbar-brand href="#"><strong>Jooti</strong>Digital</b-navbar-brand>
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="ms-auto">
                     <b-nav-item to="/">Home</b-nav-item>
-                    <b-nav-item to="/about">About</b-nav-item>
-                    <b-nav-item to="/services">Services</b-nav-item>
-                    <b-nav-item to="/portfolio">Portfolio</b-nav-item>
+                    <b-nav-item to="/#about">About</b-nav-item>
+                    <b-nav-item to="/#services">Services</b-nav-item>
+                    <b-nav-item to="/#featured-work">Portfolio</b-nav-item>
                     <b-nav-item to="/contact">Contact Us</b-nav-item>
                 </b-navbar-nav> <!-- ./b-navbar-nav -->
             </b-collapse> <!-- ./b-collapse -->
