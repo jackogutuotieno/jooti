b4835734d220ac484074c323e01c74a59957bb50
Jack Otieno <jack.otieno@ymail.com>
2021-06-23 21:01:46 +0300

    Restructured menu to capture array data

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,13 +5,40 @@
             <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
             <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav class="ms-auto">
-                    <b-nav-item to="/">Home</b-nav-item>
-                    <b-nav-item to="/#about">About</b-nav-item>
-                    <b-nav-item to="/#services">Services</b-nav-item>
-                    <b-nav-item to="/#portfolio">Portfolio</b-nav-item>
-                    <b-nav-item to="/#contact">Contact Us</b-nav-item>
+                    <b-nav-item v-for="menu in menus" :key="menu.id" :to="menu.url">{{ menu.text }}</b-nav-item>
                 </b-navbar-nav> <!-- ./b-navbar-nav -->
             </b-collapse> <!-- ./b-collapse -->
         </b-navbar> <!-- ./b-navbar -->
     </div> <!-- ./div -->
 </template> <!-- ./template -->
+
+<script>
+    export default {
+        data () {
+            return {
+                menus: [
+                    { 
+                        url: '/',
+                        text: 'Home'
+                    },
+                    { 
+                        url: '/#about',
+                        text: 'About'
+                    },
+                    { 
+                        url: '/#services',
+                        text: 'Services'
+                    },
+                    { 
+                        url: '/#portfolio',
+                        text: 'Portfolio'
+                    },
+                    { 
+                        url: '/#contact',
+                        text: 'Contact'
+                    },
+                ],
+            }
+        }
+    }
+</script>
